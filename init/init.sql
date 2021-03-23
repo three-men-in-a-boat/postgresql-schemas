0baeb79fa969daaaa61fd1092c601a53ed9d0e1a
Nicholas Eskov <mr.eskov1@yandex.ru>
2021-03-23 04:49:46 +0300

    users schema update

--- init/init.sql
+++ init/init.sql
@@ -5,19 +5,14 @@ CREATE EXTENSION IF NOT EXISTS CITEXT;
 
 CREATE TABLE users
 (
-    id                    BIGSERIAL PRIMARY KEY                              NOT NULL UNIQUE,
-    mail_user_id          VARCHAR(128)                                       NOT NULL UNIQUE CHECK ( mail_user_id <> '' ),
-    mail_user_email       VARCHAR(512)                                       NOT NULL UNIQUE CHECK ( mail_user_email <> '' ),
+    id                 BIGSERIAL PRIMARY KEY                              NOT NULL UNIQUE,
+    telegram_user_id   BIGINT                                             NOT NULL UNIQUE,
 
-    mail_access_token     VARCHAR(128)                                       NOT NULL UNIQUE CHECK ( mail_access_token <> '' ),
-    mail_refresh_token    VARCHAR(128)                                       NOT NULL UNIQUE CHECK ( mail_refresh_token <> '' ),
-    mail_token_expires_in TIMESTAMP WITH TIME ZONE                           NOT NULL,
+    mail_user_id       VARCHAR(128) UNIQUE CHECK ( mail_user_id <> '' ),
+    mail_user_email    VARCHAR(512) UNIQUE CHECK ( mail_user_email <> '' ),
+    mail_refresh_token VARCHAR(128) UNIQUE CHECK ( mail_refresh_token <> '' ),
 
-    telegram_user_id      BIGINT                                             NOT NULL UNIQUE,
-
-    created_at            TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
---     nickname         VARCHAR(256)          NOT NULL,
---     fullname         VARCHAR(512)          NOT NULL,
+    created_at         TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE events
