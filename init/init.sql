7bd88c1212484577baaed38b635626d8d406dc02
Nikolay Eskov <mr.eskov1@yandex.ru>
2021-05-07 09:33:04 +0300

    Added CHECK constraint to telegram_user_id field in users table

--- init/init.sql
+++ init/init.sql
@@ -5,7 +5,7 @@ CREATE EXTENSION IF NOT EXISTS CITEXT;
 CREATE TABLE users
 (
     id                 BIGSERIAL PRIMARY KEY                              NOT NULL UNIQUE,
-    telegram_user_id   BIGINT                                             NOT NULL UNIQUE,
+    telegram_user_id   BIGINT                                             NOT NULL UNIQUE CHECK ( telegram_user_id <> 0 ),
 
     mail_user_id       VARCHAR(128)                                       NOT NULL UNIQUE CHECK ( mail_user_id <> '' ),
     mail_user_email    VARCHAR(512)                                       NOT NULL UNIQUE CHECK ( mail_user_email <> '' ),
