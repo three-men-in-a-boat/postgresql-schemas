a466d137e4023feec77a5051a7893a85feae4d2a
Nikolay Eskov <mr.eskov1@yandex.ru>
2021-05-21 19:31:32 +0300

    Added telegram_user_timezone field

--- init/init.sql
+++ init/init.sql
@@ -4,12 +4,14 @@ CREATE EXTENSION IF NOT EXISTS CITEXT;
 
 CREATE TABLE users
 (
-    id                 BIGSERIAL PRIMARY KEY                              NOT NULL UNIQUE,
-    telegram_user_id   BIGINT                                             NOT NULL UNIQUE CHECK ( telegram_user_id <> 0 ),
+    id                     BIGSERIAL PRIMARY KEY                              NOT NULL UNIQUE,
 
-    mail_user_id       VARCHAR(128)                                       NOT NULL UNIQUE CHECK ( mail_user_id <> '' ),
-    mail_user_email    VARCHAR(512)                                       NOT NULL UNIQUE CHECK ( mail_user_email <> '' ),
-    mail_refresh_token VARCHAR(128)                                       NOT NULL UNIQUE CHECK ( mail_refresh_token <> '' ),
+    telegram_user_id       BIGINT                                             NOT NULL UNIQUE CHECK ( telegram_user_id <> 0 ),
+    telegram_user_timezone VARCHAR(128) CHECK ( telegram_user_timezone <> '' ),
 
-    created_at         TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+    mail_user_id           VARCHAR(128)                                       NOT NULL UNIQUE CHECK ( mail_user_id <> '' ),
+    mail_user_email        VARCHAR(512)                                       NOT NULL UNIQUE CHECK ( mail_user_email <> '' ),
+    mail_refresh_token     VARCHAR(128)                                       NOT NULL UNIQUE CHECK ( mail_refresh_token <> '' ),
+
+    created_at             TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
